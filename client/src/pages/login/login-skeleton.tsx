eb6f3669f41716ec453283d6cec4de278a0c5f57
angelozdev <angelozam17@gmail.com>
2025-03-13 13:00:34 -0500

    🖍️ ui: update login skeleton component with improved spacing and responsive width for better layout

--- client/src/pages/login/login-skeleton.tsx
+++ client/src/pages/login/login-skeleton.tsx
@@ -3,8 +3,8 @@ import { flex } from "~/styled-system/patterns";
 
 function InputSkeleton() {
   return (
-    <div className={flex({ direction: "column", gap: 1 })}>
-      <Skeleton.Rectangle height={16} width={100} />
+    <div className={flex({ direction: "column", gap: 2 })}>
+      <Skeleton.Rectangle height={16} width={"40%"} />
       <Skeleton.Rectangle height={48} />
     </div>
   );
@@ -12,12 +12,12 @@ function InputSkeleton() {
 
 function LoginSkeleton() {
   return (
-    <div className={flex({ direction: "column", gap: 4 })}>
+    <div className={flex({ direction: "column", gap: 4, p: 4, my: 8 })}>
       <div className={flex({ justifyContent: "center" })}>
         <Skeleton.Square size={150} />
       </div>
 
-      <div className={flex({ direction: "column", gap: 4 })}>
+      <div className={flex({ direction: "column", gap: 4, mb: 4 })}>
         <InputSkeleton />
         <InputSkeleton />
       </div>
