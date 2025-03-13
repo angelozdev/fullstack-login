22c7b1b595021fb38e9489f7781adfe7dc1f01a1
angelozdev <angelozam17@gmail.com>
2025-03-12 19:42:14 -0500

    ✨ chore: remove vercel.json configuration; update axios base URL for production and modify build script to clean dist directory before compilation

--- client/src/libs/axios.ts
+++ client/src/libs/axios.ts
@@ -2,7 +2,7 @@ import Axios from "axios";
 import LocalStorage, { LSKeys } from "./ls";
 
 const axios = Axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://fullstack-login-backend.onrender.com/api",
 });
 
 axios.interceptors.request.use(
